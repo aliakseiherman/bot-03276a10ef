fa41395148c2cc7342c3f4439a29548a8042cf52
Alex Herman <aliaksei.herman@gmail.com>
2023-06-23 13:27:12 +0300

    .

--- app.js
+++ app.js
@@ -10,98 +10,130 @@ let corsOptions = {
     origin: ['http://localhost:3000', process.env['CORS1']],
 }
 
-let isOnline = false
-let handshake = false
+let state = {}
+let handshake = {}
 
-let queue = []
+let queue = {}
 
-const xmpp = client({
-    service: process.env['SERVICE'],
-    domain: process.env['DOMAIN'],
-    resource: process.env['RESOURCE'],
-    username: process.env['USER'],
-    password: process.env['PASSWORD'],
-})
+const credentials = [1, 2].map(n => ({
+    service: process.env[`SERVICE_${n}`],
+    domain: process.env[`DOMAIN_${n}`],
+    resource: process.env[`RESOURCE_${n}`],
+    username: process.env[`USER_${n}`],
+    password: process.env[`PASSWORD_${n}`],
+    from: process.env[`FROM_${n}`],
+    to: process.env[`TO_${n}`],
+}))
 
-xmpp.on('error', (err) => {
-    console.error(err)
-})
+const build = ({ service, domain, resource, username, password, from, to }) => {
+    state[domain] = false
+    handshake[domain] = false
+    queue[domain] = []
 
-xmpp.on('offline', () => {
-    isOnline = false
-    console.log('offline')
-})
+    const xmpp = client({
+        service,
+        domain,
+        resource,
+        username,
+        password
+    })
 
-xmpp.on('stanza', async (stanza) => {
+    xmpp.on('error', (err) => {
+        console.error(err)
+    })
 
-})
+    xmpp.on('offline', () => {
+        state[domain] = false
+        console.log('offline')
+    })
 
-xmpp.on('online', async (address) => {
-    isOnline = true
-    handshake = true
+    xmpp.on('stanza', async (stanza) => {
 
-    await xmpp.send(xml('presence'))
-})
+    })
 
-xmpp.start().catch()
+    xmpp.on('online', async (address) => {
+        state[domain] = true
+        handshake[domain] = true
+
+        await xmpp.send(xml('presence'))
+    })
+
+    setInterval(async () => {
+        if (!handshake[domain]) return
+
+        if (state[domain]) {
+            await xmpp.send(xml('presence'))
+            await sendSelf('llo')
+        }
+    }, 60000)
+
+    const sendSelf = async (text) => {
+        const message = xml(
+            'message',
+            { type: 'chat', to: from },
+            xml('body', {}, text),
+        )
+        await xmpp.send(message)
+    }
+
+    return {
+        xmpp,
+        from,
+        to,
+        domain
+    }
+}
+
+const clients = credentials.map(server => build(server))
+clients.forEach(c => (c.xmpp.start().catch()))
+
+
+const send_ = async (text, { xmpp, to }) => {
+    const message = xml(
+        'message',
+        { type: 'chat', to: to },
+        xml('body', {}, text),
+    )
+
+    await xmpp.send(message)
+}
 
 const send = async (text) => {
-    const message = xml(
-        'message',
-        { type: 'chat', to: process.env['TO'] },
-        xml('body', {}, text),
-    )
-    await xmpp.send(message)
-}
-
-const sendSelf = async (text) => {
-    const message = xml(
-        'message',
-        { type: 'chat', to: process.env['FROM'] },
-        xml('body', {}, text),
-    )
-    await xmpp.send(message)
+    clients.forEach((async c => {
+        if (state[c.domain]) {
+            await send_(text, c)
+        } else {
+            queue[c.domain].push(message)
+        }        
+    }))
 }
 
 setInterval(async () => {
-    if (!handshake) return
+    clients.forEach(async c => {
+        if (!handshake[c.domain]) return
 
-    if (queue.length > 0 && isOnline) {
-        try {
-            let message = queue.shift()
-            await send(message)
-        } catch (err) {
-            console.error(err)
+        if (queue[c.domain].length > 0 && state[c.domain]) {
+            try {
+                let message = queue[c.domain].shift()
+                await send_(message, c)
+            } catch (err) {
+                console.error(err)
+            }
         }
-    }
+    })
 }, 1000)
-
-setInterval(async () => {
-    if (!handshake) return
-
-    if (isOnline) {
-        await xmpp.send(xml('presence'))
-        await sendSelf('llo')
-    }
-}, 60000)
-
 
 const app = express()
 app.use(bodyParser.json())
 app.use(cors(corsOptions))
 
 app.get('/', async (req, res) => {
-    res.status(200).send(`Online :::: ${isOnline} ${JSON.stringify(queue)}`)
+    res.status(200).send(`Online :::: ${JSON.stringify(state)} ${JSON.stringify(queue)}`)
 })
 
 app.post('/send', async (req, res) => {
     res.sendStatus(200)
-
-    if (isOnline) {
-        await send(req.body['message'])
-    } else {
-        queue.push(message)
-    }
+    await send(req.body['message'])
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
